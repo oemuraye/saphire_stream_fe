1994d53cddcd8c257179ff8a69bf7ba92d2f5f78
oemuraye <oemuraye@gmail.com>
2024-05-27 01:14:34 +0100

    arranged files

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,7 +1,16 @@
-import { Telegraf } from 'telegraf';
+const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-const web_link = "";
+const BOT_TOKEN = "7125399435:AAE_Z0cpNa6jzkT-onjuy1ZLXh4UhBWpO3Q";
 
-bot.start((ctx) => ctx.reply('Welcome'));
+const bot = new Telegraf(BOT_TOKEN);
+const web_link = "https://saphirestreamapp.com/";
+
+bot.start((ctx) =>
+ctx.reply("Welcome ", {
+        reply_markup: {
+        keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+        },
+    })
+);
+
 bot.launch();
