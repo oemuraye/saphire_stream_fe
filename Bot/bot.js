36ff84c2b5e8f0359f31e0f60ff74d124ccb7be3
oemuraye <oemuraye@gmail.com>
2024-05-27 21:57:21 +0100

    edited bot welcome info

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,26 +1,38 @@
 const { Telegraf } = require('telegraf');
+const path = require('path');
 
 const BOT_TOKEN = "";
 
 const bot = new Telegraf(BOT_TOKEN);
 const web_link = "https://saphirestreamapp.com/";
 
+const imagePath = path.join(__dirname, '../src/utils/images/sapphire coin.png');
+
 bot.start((ctx) => {
-    ctx.replyWithHTML(
-        "<b>Welcome to Saphire Stream!</b>\n\n" +
-        "Explore our <a href='https://saphirestreamapp.com/'>web app</a> for more features.\n" +
-        "Click the buttons below to get started:",
-        {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: "Open Web App", web_app: { url: web_link } }],
-                    [{ text: "Help", callback_data: 'help' }],
-                    [{ text: "Contact Us", url: 'https://saphirestreamapp.com/contact' }]
-                ]
+    const user = ctx.from;
+    const username = user.username;
+    console.log(user);
+
+    ctx.replyWithPhoto(
+        { source: imagePath }, 
+        { caption: `<b>Welcome ${username}! to Saphire Stream!</b>\n\nExplore our web app for more features.`, parse_mode: 'HTML' }
+    ).then(() => {
+        // Send the text message with buttons after the image
+        ctx.replyWithHTML(
+            "Click the buttons below to get started:",
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{ text: "Open Web App", web_app: { url: web_link } }],
+                        [{ text: "Join Our Community", url: 'https://saphirestreamapp.com/contact' }],
+                        [{ text: "Help", callback_data: 'help' }]
+                    ]
+                }
             }
-        }
-    );
+        );
+    });
 });
+
 
 // Handle the 'Help' button callback
 bot.action('help', (ctx) => {
