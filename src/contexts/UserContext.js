9f89f72661ff01cb32172e7a54353afde0db12fc
oemuraye <oemuraye@gmail.com>
2024-06-06 09:41:00 +0100

    test

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -35,7 +35,8 @@ export const UserProvider = ({ children }) => {
         const initData = telegram.initData || "";
         const user = parseTelegramInitData(initData);
         // setUserInfo(user);
-        alert(user);
+        alert(user.id);
+        alert(initData.id);
         // const userId = initData.user;
         const userId = "fds704222354";
 
