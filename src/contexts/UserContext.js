58f17cf7b080b1b316868bbff8b9f8dc11689400
oemuraye <oemuraye@gmail.com>
2024-06-08 22:41:51 +0100

    test

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -45,7 +45,7 @@ export const UserProvider = ({ children }) => {
         // const { userInfo } = parseTelegramInitData(initData);
         // const { referralCode } = parseTelegramInitData(initData);
         // let referralID = referralCode
-        const userId = "dsbcie704222354";
+        const userId = "vcxs704222354";
         
           try {
             // Fetch user data
