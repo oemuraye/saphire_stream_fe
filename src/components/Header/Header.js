88df9a1ac821dd9328d0f73fbb247c12b7b90a52
oemuraye <oemuraye@gmail.com>
2024-07-08 22:18:52 +0100

    general task header fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   const navigate = useNavigate();
 
 
-  const showGoBackArrow = location.pathname !== '/general_task' || location.pathname === '/join_socials' || location.pathname === '/connect_wallet' || location.pathname === '/trophy';
+  const showGoBackArrow = location.pathname === '/general_task' || location.pathname === '/join_socials' || location.pathname === '/connect_wallet' || location.pathname === '/trophy';
 
   return (
     <header className='container shadow-sm'>
